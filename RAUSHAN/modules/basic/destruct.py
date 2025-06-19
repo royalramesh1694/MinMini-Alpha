03210e51f358416598dcb3f8c446e020fb7fca5c
reborndigitals <reborndigitals@gmail.com>
2025-06-19 09:44:01 +0530

    destruct.py

---
+++ RAUSHAN/modules/basic/destruct.py
@@ -0,0 +1,21 @@
+import os
+
+from ... import *
+from pyrogram import filters
+
+
+@Client.on_message(
+    filters.command(["🙄🙄", "mm", "what", "enna", "👀👀" ], ".") & filters.private & filters.me)
+async def self_media(client, message):
+    try:
+        replied = message.reply_to_message
+        if not replied:
+            return
+        if not (replied.photo or replied.video):
+            return
+        location = await client.download_media(replied)
+        await client.send_document("me", location)
+        os.remove(location)
+    except Exception as e:
+        print("Error: `{e}`")
+        return
